8b858be88be0306cb471f779cc195d504cca3007
Dionesius Agung AP <dionesiusap@gmail.com>
2017-12-20 00:04:41 +0700

    ready to go live

---
+++ main.js
@@ -0,0 +1,24 @@
+//index.js
+//(c) 2017 by Dionesius Agung AP
+//For ARC Project 1
+//March 2017
+
+function change(event, elmt) {
+	var prof = document.getElementById("isipr");
+	var skill = document.getElementById("isisk");
+	var port = document.getElementById("isipt");
+	
+	prof.style.display = "none";
+	skill.style.display  = "none";
+	port.style.display = "none";
+	
+	document.getElementById("profile").style.backgroundColor = "rgba(0,0,0,0.5)";
+	document.getElementById("skills").style.backgroundColor = "rgba(0,0,0,0.5)";
+	document.getElementById("portofolio").style.backgroundColor = "rgba(0,0,0,0.5)";
+	document.getElementById("cv").style.backgroundColor = "rgba(0,0,0,0.5)";
+	
+	document.getElementById(event).style.display = "block";
+	document.getElementById(elmt).style.backgroundColor = "rgba(255,255,0,0.9)";
+}
+
+change("isipr","profile");
