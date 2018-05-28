49de0ce1d02edb303ac461929031b82e7daa5699
dionesiusap <dionesiusap@gmail.com>
2018-05-29 02:15:34 +0700

    fixed typos

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function change(event, elmt) {
 	
 	document.getElementById("profile").style.backgroundColor = "rgba(0,0,0,0.5)";
 	document.getElementById("skills").style.backgroundColor = "rgba(0,0,0,0.5)";
-	document.getElementById("portofolio").style.backgroundColor = "rgba(0,0,0,0.5)";
+	document.getElementById("portfolio").style.backgroundColor = "rgba(0,0,0,0.5)";
 	document.getElementById("cv").style.backgroundColor = "rgba(0,0,0,0.5)";
 	
 	document.getElementById(event).style.display = "block";
